1a470620ef483986186637af415a6d9b13c5485c
tcarreira <tiago.g.carreira+git@gmail.com>
2020-04-06 10:21:21 -0300

    first test

--- main.go
+++ main.go
@@ -6,12 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.Default()
+func setRoutes(r *gin.Engine) *gin.Engine {
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
 	})
 
-	r.Run()
+	return r
+}
+
+func setupRouter() *gin.Engine {
+	r := gin.Default()
+
+	return setRoutes(r)
+}
+
+func main() {
+	setupRouter().Run()
 }
---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func setupTestRouter() *gin.Engine {
+	gin.SetMode(gin.TestMode)
+	r := gin.New()
+	return setRoutes(r)
+}
+
+func performRequest(r http.Handler, method, path string) *httptest.ResponseRecorder {
+	req, _ := http.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func TestGoHelloWorld(t *testing.T) {
+	router := setupTestRouter()
+
+	w := performRequest(router, "GET", "/")
+
+	// Convert the JSON response to a map
+	var response map[string]string
+	json.Unmarshal([]byte(w.Body.String()), &response)
+
+	assert.EqualValues(
+		t,
+		map[string]string{"data": "hello world"},
+		response,
+	)
+
+}
