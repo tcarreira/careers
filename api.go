10dc10ce261f7f6bc352b9766e999fc09e073a3f
tcarreira <tiago.g.carreira+git@gmail.com>
2020-04-09 21:30:29 -0300

    move routes.go to api.go

--- api.go
+++ api.go
@@ -218,3 +218,51 @@ func (api *SuperAPI) groupsDeleteHandler(c *gin.Context) {
 		"Not implemented", "out of the scope for this exercise",
 	})
 }
+
+//  Routes
+
+func setRoutes(s *Server) *gin.Engine {
+
+	s.Router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+
+	v1 := s.Router.Group("/api/v1")
+	{
+		api := SuperAPI{
+			DB:     s.DB,
+			Router: s.Router,
+		}
+
+		v1.POST("/super-hero", api.superHeroPOSTHandler)
+		v1.POST("/super-vilan", api.superVilanPOSTHandler)
+
+		v1.POST("/supers", api.supersPOSTHandler)
+		v1.GET("/supers", api.supersGETHandler)
+		v1.GET("/supers/:id", api.supersGETHandler)
+		v1.PUT("/supers/:id", api.supersPUTHandler)
+		v1.DELETE("/supers/:id", api.supersDeleteHandler)
+
+		v1.POST("/groups", api.groupsPOSTHandler)
+		v1.GET("/groups/:name", api.groupsGETHandler)
+		v1.PUT("/groups/:name", api.groupsPUTHandler)
+		v1.DELETE("/groups/:name", api.groupsDeleteHandler)
+	}
+
+	return s.Router
+}
+
+// run server
+
+func (s *Server) setupRouter() *Server {
+	s.Router = gin.Default()
+
+	s.Router = setRoutes(s)
+
+	return s
+}
+
+func (s *Server) runHTTPServer() {
+	s.setupRouter()
+	s.Router.Run()
+}
